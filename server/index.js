5c0b83bbe3221feaa14c7b91af7210f73a46a5cd
Chris Manson <chris@stonecircle.io>
2018-07-05 16:34:54 +0100

    implementing significantly nicer revisions page

--- server/index.js
+++ server/index.js
@@ -6,8 +6,8 @@ const { promisify } = require('util');
 
 const client = redis.createClient(nconf.get('redis'));
 
-const zrangeAsync = promisify(client.zrangeAsync).bind(client);
-const getAsync = promisify(client.getAsync).bind(client);
+const zrangeAsync = promisify(client.zrange).bind(client);
+const getAsync = promisify(client.get).bind(client);
 
 module.exports = (app) => {
   // Log proxy requests
@@ -18,7 +18,22 @@ module.exports = (app) => {
   app.get('/revisions', async (req, res) => {
     const revisions = await zrangeAsync(`${nconf.get('lightning:appName')}:index:revisions`, 0, -1);
 
-    res.render('revisions.hbs', { revisions });
+    const revisionDataPromises = revisions.map(async (revision) => {
+      let data = await getAsync(`${nconf.get('lightning:appName')}:index:revision-data:${revision}`);
+
+      let parsedData = JSON.parse(data)
+
+      return {
+        ...parsedData,
+        shortSha: parsedData.scm.sha.substring(0, 7),
+      }
+    });
+
+    const revisionData = await Promise.all(revisionDataPromises);
+
+    console.log(revisionData);
+
+    res.render('revisions.hbs', { revisionData });
   });
 
   // fall back to shipping the default index.html
